29581090b724efe82d77dc6784d2a8f3b94c341d
Steff-C <stephanie.core@mortenson.com>
2021-04-29 22:58:19 -0500

    updated ipynb and created connecttion to Postgres

--- elections_db.sql
+++ elections_db.sql
@@ -1,20 +1,4 @@
-CREATE TABLE "election_data" (
-    "year" INT   NOT NULL,
-    "state" VARCHAR   NOT NULL,
-    "state_po" VARCHAR   NOT NULL,
-    "county" VARCHAR   NOT NULL,
-    "office" VARCHAR   NOT NULL,
-    "candidate" VARCHAR   NOT NULL,
-	"party" VARCHAR   NOT NULL,
-    "candidate_votes" INT   NOT NULL,
-    "total_votes_county" INT NOT NULL
-);
-
-select * from election_data;
-
-drop table election_data;
-
-create table election_data (
+Create table election_data (
 	index int primary key,
 	year INT   NOT NULL,
     state VARCHAR   NOT NULL,
@@ -23,7 +7,26 @@ create table election_data (
 	FIPS VARCHAR   NOT NULL,
     office VARCHAR   NOT NULL,
     candidate VARCHAR   NOT NULL,
-	party VARCHAR   NOT NULL,
+	party VARCHAR,
+    candidate_votes INT   NOT NULL,
+    total_votes_county INT NOT NULL,
+	version INT NOT NULL
+);
+
+select * from election_data;
+
+drop table election_data;
+
+Create table election_data (
+	index int primary key,
+	year INT   NOT NULL,
+    state VARCHAR   NOT NULL,
+    state_po VARCHAR   NOT NULL,
+    county VARCHAR   NOT NULL,
+	FIPS VARCHAR   NOT NULL,
+    office VARCHAR   NOT NULL,
+    candidate VARCHAR   NOT NULL,
+	party VARCHAR,
     candidate_votes INT   NOT NULL,
     total_votes_county INT NOT NULL,
 	version INT NOT NULL
