52193ea96edb065ed2268f69a0df8ef3e781ffb7
Nathan Witte <nathanwitte@Nathans-MacBook-Pro-7.local>
2021-05-04 20:01:35 -0500

    Added State Election Data

--- elections_db.sql
+++ elections_db.sql
@@ -2,22 +2,6 @@ Create table election_data (
 	index int primary key,
 	"Year" INT   NOT NULL,
     "State" VARCHAR   NOT NULL,
-    State_Abbr VARCHAR   NOT NULL,
-    County VARCHAR   NOT NULL,
-    Office VARCHAR   NOT NULL,
-    Candidate VARCHAR   NOT NULL,
-	Party VARCHAR,
-    Candidate_Votes INT   NOT NULL,
-    Total_Votes_byCounty INT NOT NULL
-);
-
-select * from election_data;
-drop table election_data;
-
-Create table election_data (
-	index int primary key,
-	"Year" INT   NOT NULL,
-    "State" VARCHAR   NOT NULL,
     "State_Abbr" VARCHAR   NOT NULL,
     "County" VARCHAR   NOT NULL,
     "Office" VARCHAR   NOT NULL,
@@ -28,3 +12,18 @@ Create table election_data (
 );
 
 select * from election_data;
+-- drop table election_data;
+
+Create table state_election_data (
+	index int primary key,
+	"Year" INT   NOT NULL,
+    "State" VARCHAR   NOT NULL,
+    "State_Abbr" VARCHAR   NOT NULL,
+    "Office" VARCHAR   NOT NULL,
+    "Candidate" VARCHAR   NOT NULL,
+    "Candidate_Votes" INT NOT NULL,
+    "Total_Votes_by_State" INT NOT NULL,
+	"Party" VARCHAR NOT NULL
+);
+select * from state_election_data;
+-- drop table state_election_data;
